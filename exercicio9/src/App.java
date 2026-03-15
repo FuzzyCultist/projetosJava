3fddaa6ddf08c1ab6f33994227c7ce7e835c0073
FuzzyCultist <andysantospupomuniz@gmail.com>
2026-03-15 19:43:54 -0300

    att do exercicio 9

--- exercicio9/src/App.java
+++ exercicio9/src/App.java
@@ -4,5 +4,14 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
        Scanner entrada = new Scanner(System.in);
+       System.out.println("Insira o valor do poduto: ");
+        double valor = entrada.nextDouble();
+       System.out.println("insira o valor entregue ao caixa");
+        double pago = entrada.nextDouble();
+        double troco = pago - valor;
+       System.out.println(String.format("o troco a ser devolvido para o cliente seria de R$%.2f", troco));
+       entrada.close();
+
+
     }
 }
